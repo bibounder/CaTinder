d736d545ffbcc3a4bc43fc9ad596a9295a68fd06
paolin_t <paolin_t@etna-alternance.net>
2015-04-22 17:23:30 +0200

    1er commit

--- CaTinder/SecondViewController.swift
+++ CaTinder/SecondViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func helloWordAction(nameTextField: UITextField) {
+        nameLabel.text = "Salut \(nameTextField.text)"
+    }
 
 }
 
